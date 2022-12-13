5b783217ca64b8a4b7d123bcded17d7f54d80ddc
joliver427@icloud.com <joliver427@icloud.com>
2022-12-13 12:01:21 -0700

    commit tree 16b144a8936fc80b35abfdae9db633e68fc12237

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result
+  if (number <= 400) {
+    result = "This one is on me!";
+  }
+  else if (number > 400 && number <= 2000) {
+    result = "That will be twenty bucks.";
+  }
+  else if (number > 2000 && number <=2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  if (city === "NYC") {
+    result = "Ok, sounds good.";
+  }
+  else {
+    result = "No go."
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
